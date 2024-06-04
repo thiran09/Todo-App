83c72581bd57543d68e004cb5f795b867a8872b6
Thiran Sasanka <thiransasanka1@gmail.com>
2024-06-04 11:28:55 +0530

    d

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -8,7 +8,7 @@ export default function Todo() {
   return (
     <div>
       <Form todoList={todoList} setTodoList={setTodoList}  />
-      <TodoDisplay todoList={todoList} />
+      <TodoDisplay todoList={todoList} setTodoList={setTodoList}/>
     </div>
   );
 }
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React from "react";
 import styles from "../css/todoitems.module.css";
 
-export default function TodoItems({item}) {
+export default function TodoItems({ item, todoList, setTodoList }) {
+  
+  function handleDelete(item) {
+    console.log(item, "deleted");
+    console.log((todoList.filter((todo) => todo !== item)));
+
+  }
+
   return (
     <div className={styles.itemsdiv}>
-        <div className={styles.items}>
-          <span className={styles.span} >{item.name}</span>
-          <button className={styles.button} >Delete</button>
-                
-        </div>
+      <div className={styles.items}>
+        <span className={styles.span}>{item}</span>
+        <button onClick={() => handleDelete(item)} className={styles.button}>
+          Delete
+        </button>
+      </div>
     </div>
-  )
+  );
 }
